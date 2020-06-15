eb5233c7ca55ba80bd74313f970157fd7c6fea13
Andrey Sapeshko <andrdd17@gmail.com>
2020-06-15 06:06:00 +0300

    Урок № 8

--- core/src/ru/sapeshkoas/dunegame/core/users_logic/AiLogic.java
+++ core/src/ru/sapeshkoas/dunegame/core/users_logic/AiLogic.java
@@ -51,7 +51,7 @@ public class AiLogic extends BaseLogic {
             }
             for (int i = 0; i < tmpAiHarvester.size(); i++) {
                 Harvester aiHarvester = tmpAiHarvester.get(i);
-                aiHarvesterProcessing(aiHarvester);
+
             }
         }
     }
@@ -82,96 +82,5 @@ public class AiLogic extends BaseLogic {
         }
     }
 
-    public void aiHarvesterProcessing(AbstractUnit aiUnit) {
-        if (gc.getBattleMap().getResourceCount(aiUnit.getPosition()) < 1) {
-            Vector2 target = scanAroundCells(aiUnit, 1);
-            if (target != null) {
-                aiUnit.commandMoveTo(target);
-                return;
-            }
-//            if (aiUnit.getPosition().dst(aiUnit.getDestination()) < 5) {
-//                float[][] resourceMap = gc.getBattleMap().getResourceMap();
-//                float maxResource = 0.0f;
-//                for (int i = 0; i < resourceMap.length; i++) {
-//                    for (int j = 0; j < resourceMap[i].length; j++) {
-//                        if (resourceMap[i][j] > maxResource) {
-//                            aiUnit.getTmp().set(i * BattleMap.CELL_SIZE + BattleMap.CELL_SIZE * 2,
-//                                    j * BattleMap.CELL_SIZE + BattleMap.CELL_SIZE * 2);
-//                            if (isFreeTarget(aiUnit.getTmp())) {
-//                                aiUnit.commandMoveTo(aiUnit.getTmp());
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-        }
-    }
-
-    public boolean isFreeTarget(Vector2 target) {
-        for (int i = 0; i < tmpAiHarvester.size(); i++) {
-            Harvester h = tmpAiHarvester.get(i);
-            if (target.dst(h.getPosition()) < 200.0f) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public Vector2 scanAroundCells(AbstractUnit aiUnit, int radius) {
-        int cellX = aiUnit.getCellX() - radius;
-        int cellY = aiUnit.getCellY() - radius;
-        if (cellX < 0) {
-            cellX = 0;
-        }
-        if (cellY < 0) {
-            cellY = 0;
-        }
-        int endScanX = aiUnit.getCellX() + radius;
-        int endScanY = aiUnit.getCellY() + radius;
-        if (endScanX >= BattleMap.COLUMN_COUNT) {
-            endScanX = BattleMap.COLUMN_COUNT - 1;
-        }
-        if (endScanY >= BattleMap.ROWS_COUNT) {
-            endScanY = BattleMap.ROWS_COUNT - 1;
-        }
-        int resource = 0;
-        tmp = null;
-        for (int i = cellX; i <= endScanX; i++) {
-            for (int j = cellY; j <= endScanY; j++) {
-                int cellResource = gc.getBattleMap().getResourceCount(aiUnit.getTmp().set(i * BattleMap.CELL_SIZE + BattleMap.CELL_SIZE / 2,
-                        j * BattleMap.CELL_SIZE + BattleMap.CELL_SIZE / 2));
-                if (cellResource > resource) {
-                    tmp = aiUnit.getTmp();
-                    resource = cellResource;
-                }
-            }
-        }
-        return tmp;
-    }
-
-    public void freeMove(AbstractUnit aiUnit, float distance) {
-        float k = 1.0f;
-        if (MathUtils.random() < 0.5f) {
-            k = - 1.0f;
-        }
-        float x = aiUnit.getPosition().x + k * distance;
-        if (MathUtils.random() < 0.5f) {
-            k = - 1.0f;
-        }
-        float y = aiUnit.getPosition().y + k * distance;
-        if (x > 1240) {
-            x = 1240.0f;
-        }
-        if (x < 40) {
-            x = 40.0f;
-        }
-        if (y > 680) {
-            y = 680.0f;
-        }
-        if (y < 40) {
-            y = 40.0f;
-        }
-        aiUnit.commandMoveTo(aiUnit.getTmp().set(x, y));
-    }
 
 }
